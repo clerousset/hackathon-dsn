551d52ae992192b2b78432f349f2c6a0c9ac01ad
clerousset <clerousset@protonmail.com>
2024-05-14 16:53:07 +0200

    Update etude_sequence.R

--- etude_sequence.R
+++ etude_sequence.R
@@ -3,9 +3,16 @@ library(questionr)
 
 # visiblement les données sont pas oufs avant 2021 et après mai 2023
 base_finale_reduit <- base_finale[annee >= 2021 & !(annee == 2023 & mois > 5)]
+id_assure_ayant_chgt <- base_finale_reduit[, .(modif=max(rleid(statut))), by = "id_assure"][modif >1]$id_assure
+base_finale_reduit <- base_finale_reduit[id_assure %in% id_assure_ayant_chgt]
+
+# on vire les trop jeunes et trop vieux
+base_id[,annee_naissance := as.integer(substring(date_naissance, 5))]
+base_id[, categorie := fcase(annee_naissance >1996, "jeune", annee_naissance < 1960, "vieux", default = "normal")]
+base_finale_reduit <- base_id[,.(id_assure, categorie)][base_finale_reduit, on ="id_assure"]
 
 suite <- seqdef(
-  data = dcast(base_finale_reduit, id_assure ~ annee+mois )[,!"id_assure"])
+  data = dcast(base_finale_reduit[categorie =="normal", !c("modif","categorie")], id_assure ~ annee + mois )[,!"id_assure"])
 suite.om = seqdist(suite, method ="LCS")
 
 suite.dist <- hclust(as.dist(suite.om), method = "ward.D2")
@@ -25,4 +32,4 @@ seqmsplot(suite, group = suite.part)
 seqrplot(suite, group =suite.part, dist.matrix = suite.om, criterion = "dist")
 
 freq(suite.part)
-cprop(table(suite.part, base_id$sexe))
+cprop(table(suite.part, base_id[categorie=="normal"]$sexe))
